f1313fb2ebc336ba56dda0faa498488fe8914b13
Clement Azalbert <clementazalbert04@gmail.com>
2022-04-19 22:17:22 +0200

    added funct getResult

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const resultDisplay = document.getElementById('result');
 const choices = document.querySelectorAll('button');
 let userChoice;
 let computerChoice;
+let result;
 
 choices.forEach(choice => choice.addEventListener('click', (event) => {
   userChoice = event.target.id;
   UserChoiceDisplay.innerHTML = userChoice;
   generateComputerChoice()
+  getResult();
 }))
 
 function generateComputerChoice() {
@@ -24,4 +26,29 @@ function generateComputerChoice() {
     computerChoice = 'paper'
   }
   ComputerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = `It's a draw!`;
+  }
+  if (computerChoice === 'rock' && userChoice === 'paper') {
+    result = `You win!`
+  }
+  if (computerChoice === 'rock' && userChoice === 'scissors') {
+    result = `You lost!`
+  }
+  if (computerChoice === 'paper' && userChoice === 'scissors') {
+    result = `You win!`
+  }
+  if (computerChoice === 'paper' && userChoice === 'rock') {
+    result = `You lost!`
+  }
+  if (computerChoice === 'scissors' && userChoice === 'rock') {
+    result = `You win!`
+  }
+  if (computerChoice === 'scissors' && userChoice === 'paper') {
+    result = `You lost!`
+  }
+  resultDisplay.innerHTML = result;
 }
