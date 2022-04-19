7ae43dee7f674a7394470914476f2f7a4dda061b
Clement Azalbert <clementazalbert04@gmail.com>
2022-04-19 22:05:59 +0200

    added funct generateComputerChoice

--- app.js
+++ app.js
@@ -3,8 +3,25 @@ const UserChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
 const choices = document.querySelectorAll('button');
 let userChoice;
+let computerChoice;
 
 choices.forEach(choice => choice.addEventListener('click', (event) => {
   userChoice = event.target.id;
   UserChoiceDisplay.innerHTML = userChoice;
+  generateComputerChoice()
 }))
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+  if (randomNumber === 1) {
+    computerChoice = 'rock'
+  }
+  if (randomNumber === 2) {
+    computerChoice = 'scissors'
+  }
+  if (randomNumber === 3) {
+    computerChoice = 'paper'
+  }
+  ComputerChoiceDisplay.innerHTML = computerChoice;
+}
